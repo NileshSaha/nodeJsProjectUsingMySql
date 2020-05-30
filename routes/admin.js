0eb4372d8cbca6e575a5ff2e013659dfe1d335a9
Nilesh Saha <nilesh@codelogicx.com>
2020-05-30 11:00:39 +0530

    Initial Commit

---
+++ routes/admin.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+
+const rootDir = require('../util/path');
+const products = [];
+
+const router = express.Router();
+
+router.post('/add-product', (req, res, next) => {
+    products.push({
+        title: req.body.title
+    })
+    res.redirect('/');
+});
+
+router.get('/add-product', (req, res, next) => {
+    res.render('add-product', {
+        pageTitle: 'Add Product'
+    });
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.htm l'));
+});
+
+exports.routes = router;
+exports.products = products;

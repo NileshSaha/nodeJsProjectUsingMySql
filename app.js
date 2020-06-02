fceeb5d704c9f540e527016ec08490ceeab71f59
Nilesh Saha <nilesh@codelogicx.com>
2020-06-02 15:51:16 +0530

    pug s and handlebars removed

--- app.js
+++ app.js
@@ -6,13 +6,6 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// app.engine('hbs', expressHbs({
-//     layoutsDir: 'views/layouts',
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs'
-// }))
-// app.set('view engine', 'hbs');
-// app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -31,7 +24,6 @@ app.use((req, res, next) => {
     res.status(404).render('404', {
         pageTitle: '404',
     });
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);

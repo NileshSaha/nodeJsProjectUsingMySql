8699d84f77a92930f66cadb7c24869ac63d9b7e8
Nilesh Saha <nilesh@codelogicx.com>
2020-06-03 19:23:35 +0530

    edit updated

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,7 @@ const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
+
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
@@ -15,5 +16,7 @@ router.get('/products', adminController.getProducts);
 router.post('/add-product', adminController.postAddProduct);
 
 router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
